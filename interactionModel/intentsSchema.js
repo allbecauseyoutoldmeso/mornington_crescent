344ce83d3a0b934ef34e044c290d441dafd275fd
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-15 19:32:47 +0100

    add interaction model

---
+++ interactionModel/intentsSchema.js
@@ -0,0 +1,19 @@
+{
+  "intents": [
+    {
+      "slots": [
+        {
+          "name": "station",
+          "type": "LIST_OF_STATIONS"
+        }
+      ],
+      "intent": "PlayIntent"
+    },
+    {
+      "intent": "AMAZON.HelpIntent"
+    },
+    {
+      "intent": "AMAZON.StopIntent"
+    }
+  ]
+}
